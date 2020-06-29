9a2eca46663fa62c153f937c43fc6cdf32e48b4c
Yousuf Hasan <yousuf85@gmail.com>
2020-06-29 21:40:41 +1000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 
 
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBozd5stkhvRJR855cPs8ma92kqbh3NOTQ",
-    authDomain: "sajao-f63ea.firebaseapp.com",
-    databaseURL: "https://sajao-f63ea.firebaseio.com",
-    storageBucket: "sajao-f63ea.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
 }
 
  export const firebaseApp  = firebase.initializeApp(FIREBASE_CONFIG);
